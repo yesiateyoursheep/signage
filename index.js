8b07e563bb436fa4f1310ac5f0df0d543be3c5d0
yesiateyoursheep <yesiateyoursheep@gmail.com>
2019-08-21 10:03:24 +1200

    Implemented a static server

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var react = require('react')
 
 var app = express()
 
-app.get('/',function(req,res){
+/*app.get('/hello',function(req,res){
     res.send('hello world')
-});
+})*/
+app.use(express.static('html'))
 
 app.listen(3000,'0.0.0.0',()=>console.log('listening on port 3000...'))
