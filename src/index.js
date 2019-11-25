f74a3009991ce1f384edf0235836e0347d778c13
Yesiateyoursheep <yesiateyoursheep@gmail.com>
2019-11-25 21:57:53 +1300

    Finish weather, improve MemeDB

--- src/index.js
+++ src/index.js
@@ -87,9 +87,16 @@ function TypeRenderer(props){
 			<img src={props.url} alt="meme"/>
 		)
 	}
-	if(props.type === "video" || props.type === "webm" || props.type === "audio"){
+	if(props.type === "video" || props.type === "webm"){
 		return(
-			<video muted autoPlay>
+			<video muted autoPlay loop>
+				<source src={props.url}/>
+			</video>
+		);
+	}
+	if(props.type === "audio"){
+		return(
+			<video controls>
 				<source src={props.url}/>
 			</video>
 		);
@@ -167,7 +174,7 @@ class Weather extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			hourly: {},
+			hourly: {}, // hourly is a little misleading, it's actually once every 3 hours
 			loading: true,
 			error: false
 		}
@@ -176,7 +183,7 @@ class Weather extends React.Component{
 	componentDidMount(){
 		fetch("https://api.openweathermap.org/data/2.5/forecast?q=Nelson,nz&appid=71e65618a80b5c3d419b2ac804582791")
 		.then(response => response.json())
-		.then(data => {this.setState({hourly: data.list, loading: false}); })
+		.then(data => {this.setState({hourly: data.list.slice(0,10), loading: false}); })
 		.catch(error => this.setState({error, loading: false}));
 	}
 
@@ -185,8 +192,11 @@ class Weather extends React.Component{
 			var result = [];
 			props.hourly.map(weather => {
 				result.push(
-					<div key={weather.dt} className="card">
-						<img src={"/svg/Weather/"+weather.weather[0].main+".svg"} alt={weather.weather[0].main}/>
+					<div key={weather.dt} className="card" style={{'left':'calc( '+((weather.dt-new Date().getTime()/1000)/600).toString()+'vw - 15vh )'}}>
+						<span className="hour">{formatDate(new Date(weather.dt*1000),"dddd, htt")}</span>
+						<span className="temp">{Math.round(weather.main.temp-273.15).toString()+"°C ("+(weather.main.humidity).toString()+"%)"}</span>
+						<span className="desc">{weather.weather[0].description}</span>
+						<img src={"/svg/Weather/"+weather.weather[0].main+".svg"} alt={weather.weather[0].description}/>
 					</div>
 				)
 				return null;
@@ -321,3 +331,86 @@ setInterval(function() {
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function formatDate(date, format, utc) {
+	var MMMM = ["\x00", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var MMM = ["\x01", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var dddd = ["\x02", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	var ddd = ["\x03", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+	function ii(i, len) {
+			var s = i + "";
+			len = len || 2;
+			while (s.length < len) s = "0" + s;
+			return s;
+	}
+
+	var y = utc ? date.getUTCFullYear() : date.getFullYear();
+	format = format.replace(/(^|[^\\])yyyy+/g, "$1" + y);
+	format = format.replace(/(^|[^\\])yy/g, "$1" + y.toString().substr(2, 2));
+	format = format.replace(/(^|[^\\])y/g, "$1" + y);
+
+	var M = (utc ? date.getUTCMonth() : date.getMonth()) + 1;
+	format = format.replace(/(^|[^\\])MMMM+/g, "$1" + MMMM[0]);
+	format = format.replace(/(^|[^\\])MMM/g, "$1" + MMM[0]);
+	format = format.replace(/(^|[^\\])MM/g, "$1" + ii(M));
+	format = format.replace(/(^|[^\\])M/g, "$1" + M);
+
+	var d = utc ? date.getUTCDate() : date.getDate();
+	format = format.replace(/(^|[^\\])dddd+/g, "$1" + dddd[0]);
+	format = format.replace(/(^|[^\\])ddd/g, "$1" + ddd[0]);
+	format = format.replace(/(^|[^\\])dd/g, "$1" + ii(d));
+	format = format.replace(/(^|[^\\])d/g, "$1" + d);
+
+	var H = utc ? date.getUTCHours() : date.getHours();
+	format = format.replace(/(^|[^\\])HH+/g, "$1" + ii(H));
+	format = format.replace(/(^|[^\\])H/g, "$1" + H);
+
+	var h = H > 12 ? H - 12 : H === 0 ? 12 : H;
+	format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
+	format = format.replace(/(^|[^\\])h/g, "$1" + h);
+
+	var m = utc ? date.getUTCMinutes() : date.getMinutes();
+	format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
+	format = format.replace(/(^|[^\\])m/g, "$1" + m);
+
+	var s = utc ? date.getUTCSeconds() : date.getSeconds();
+	format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
+	format = format.replace(/(^|[^\\])s/g, "$1" + s);
+
+	var f = utc ? date.getUTCMilliseconds() : date.getMilliseconds();
+	format = format.replace(/(^|[^\\])fff+/g, "$1" + ii(f, 3));
+	f = Math.round(f / 10);
+	format = format.replace(/(^|[^\\])ff/g, "$1" + ii(f));
+	f = Math.round(f / 10);
+	format = format.replace(/(^|[^\\])f/g, "$1" + f);
+
+	var T = H < 12 ? "AM" : "PM";
+	format = format.replace(/(^|[^\\])TT+/g, "$1" + T);
+	format = format.replace(/(^|[^\\])T/g, "$1" + T.charAt(0));
+
+	var t = T.toLowerCase();
+	format = format.replace(/(^|[^\\])tt+/g, "$1" + t);
+	format = format.replace(/(^|[^\\])t/g, "$1" + t.charAt(0));
+
+	var tz = -date.getTimezoneOffset();
+	var K = utc || !tz ? "Z" : tz > 0 ? "+" : "-";
+	if (!utc) {
+			tz = Math.abs(tz);
+			var tzHrs = Math.floor(tz / 60);
+			var tzMin = tz % 60;
+			K += ii(tzHrs) + ":" + ii(tzMin);
+	}
+	format = format.replace(/(^|[^\\])K/g, "$1" + K);
+
+	var day = (utc ? date.getUTCDay() : date.getDay()) + 1;
+	format = format.replace(new RegExp(dddd[0], "g"), dddd[day]);
+	format = format.replace(new RegExp(ddd[0], "g"), ddd[day]);
+
+	format = format.replace(new RegExp(MMMM[0], "g"), MMMM[M]);
+	format = format.replace(new RegExp(MMM[0], "g"), MMM[M]);
+
+	format = format.replace(/\\(.)/g, "$1");
+
+	return format;
+};
